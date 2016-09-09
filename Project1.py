0370e852c5da67a5148ae68d3365769f496097c7
coparker <coparker@csumb.edu>
2016-09-09 16:53:49 -0700

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -1,11 +1,11 @@
 from PIL import Image
 import os
 import statistics
+# link to github account: https://github.com/coparker/CST205Project1/edit/master/Project1.py
 fp = "C:/Users/Cody/Downloads/Project1Images/" # could make user input filepath
 pic = [Image.open(fp + file) for file in os.listdir(fp)] # list comp to add images to list
 temp = Image.new('RGB', (pic[0].width, pic[0].height))
-for x in range(temp.width): # I want to make my nested for loop 1 line
-	for y in range(temp.height): # using an itertools function
-		list = [a.getpixel((x, y)) for a in pic] # adding ixel data to a list using list comp
-		temp.putpixel((x, y), statistics.median(list)) # add the pixel data to the created image
+for (x, y) in [(x, y) for x in range(temp.width) for y in range(temp.height)]: # nested for loop in 1 line
+	list = [a.getpixel((x, y)) for a in pic] # adding ixel data to a list using list comp
+	temp.putpixel((x, y), statistics.median(list)) # add the pixel data to the created image
 temp.show()
