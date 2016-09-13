7296916d444e66759744581b92e4d9bc57d8521a
coparker <coparker@csumb.edu>
2016-09-13 15:27:52 -0700

    Update Project1.py

--- Project1.py
+++ Project1.py
@@ -2,10 +2,9 @@ from PIL import Image
 import os
 import statistics
 # link to github account: https://github.com/coparker/CST205Project1/edit/master/Project1.py
-fp = "C:/Users/Cody/Downloads/Project1Images/" # could make user input filepath
-pic = [Image.open(fp + file) for file in os.listdir(fp)] # list comp to add images to list
-temp = Image.new('RGB', (pic[0].width, pic[0].height))
+fp = input("Please enter the desired filepath: ")
+pic = [Image.open(fp + file) for file in os.listdir(fp) if file.endswith(".png")] # list comp to add images to list
+temp = Image.new('RGB', (pic[0].width, pic[0].height)) # will throw error is pic is empty (file path entry error?)
 for (x, y) in [(x, y) for x in range(temp.width) for y in range(temp.height)]: # nested for loop in 1 line
-	list = [a.getpixel((x, y)) for a in pic] # adding ixel data to a list using list comp
-	temp.putpixel((x, y), statistics.median(list)) # add the pixel data to the created image
+	temp.putpixel((x, y), statistics.median([a.getpixel((x, y)) for a in pic])) # add the pixel data to the created image
 temp.show()
